30c6f555a389a5371cf771dc5d4c9b1bc157cff8
Sola <dev@sola.love>
2018-03-28 17:51:08 +0800

    compatible with 1.11.2
    Signed-off-by: Sola <dev@sola.love>

--- src/main/kotlin/love/sola/spigot/dailysign/utils/CommandSenderExtensions.kt
+++ src/main/kotlin/love/sola/spigot/dailysign/utils/CommandSenderExtensions.kt
@@ -1,6 +1,6 @@
 package love.sola.spigot.dailysign.utils
 
 import net.md_5.bungee.chat.ComponentSerializer
-import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
 
-fun CommandSender.tellraw(rawJson: String) = spigot().sendMessage(*ComponentSerializer.parse(rawJson))
+fun Player.tellraw(rawJson: String) = spigot().sendMessage(*ComponentSerializer.parse(rawJson))
